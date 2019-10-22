9b136594f7b19fb18e81e593a428751db2d2a1db
LowGinWee <notginwee@gmail.com>
2019-10-22 21:27:37 +0800

    updated friends table and forms to add and delete

--- routes/account.js
+++ routes/account.js
@@ -27,5 +27,43 @@ router.get('/', async function(req, res, next) {
     }
   });
 
+  router.get('/:user/friends', async function(req, res, next) {
+    var user = req.params.user;
+    var getFriends = "(SELECT friendUsername FROM Friends where myUsername = '" + user + "')";
+    var sql_query = "SELECT * FROM " + getFriends + " f LEFT OUTER JOIN preferences p ON (f.friendUsername = p.username)";
+    console.log(sql_query);
+    pool.query(sql_query, (err, data) => {
+        res.render('friends', { title: 'Friends', username: user, userData: data.rows});
+    });
+  });
+
+  router.get('/:user/addfriends', async function(req, res, next) {
+    var user = req.params.user;
+        res.render('addFriends', { title: 'addFriends', username: user});
+  });
+
+  router.post('/:user/:user/add', async function(req, res, next) {
+    var user = req.params.user;
+    var del = req.body.del;
+    var a = req.body.names;
+	a = a.split("\r\n");
+	var insert_query = "";
+    if (del == 'true'){
+        for (var i = 0; i < a.length; i++) {
+            insert_query += "DELETE FROM friends WHERE myusername = '"+ user + "' AND friendusername = '" + a[i] +"';\n";
+        }
+    } else {
+	    for (var i = 0; i < a.length; i++) {
+		    insert_query += "INSERT INTO friends VALUES ('"+ user + "','" + a[i] +"');\n";
+        }
+    }
+    
+    console.log(insert_query);
+
+    await pool.query(insert_query, (err, data) => {
+       res.redirect('/account/');
+    });
+});
+
   
   module.exports = router;
