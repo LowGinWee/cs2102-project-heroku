3a4082838310503e3a508812c68585cbfb35dd31
LowGinWee <notginwee@gmail.com>
2019-11-03 18:29:18 +0800

    moved triggers to initv5.1

--- initv5.1.sql
+++ initv5.1.sql
@@ -231,3 +231,39 @@ DROP TRIGGER IF EXISTS trig5 ON public.availability;
 CREATE TRIGGER trig4
 BEFORE INSERT ON availability 
 FOR EACH ROW EXECUTE PROCEDURE checkMaxTables();
+
+CREATE OR REPLACE FUNCTION getCurrTables(myRName varchar(100), myBranchID varchar(100), myReserveDate date, myReserveTime time)
+	RETURNS integer AS
+	$$ 
+	DECLARE 
+		currtables integer;
+	BEGIN
+	WITH reservedTablesCount(RName, branchID, totalreserved, reserveDate, reserveTime) AS 
+		(SELECT R.Rname, R.branchID, SUM(R.numTables) AS totalreserved, R.reserveDate, R.reserveTime
+		FROM Reservation R
+		WHERE R.reserveDate = myReserveDate 
+		AND R.reserveTime = myReserveTime
+		GROUP BY R.Rname, R.branchID, R.reserveDate, R.reserveTime)
+	SELECT A.numTables - R.totalreserved
+	INTO currtables -- set variable
+	FROM Availability A 
+	FULL JOIN reservedTablesCount R
+	ON A.RName = R.RName 
+	AND A.branchID = R.branchID
+	WHERE A.Rname = myRName 
+	AND A.branchID = myBranchID; -- rName and branchID can be specified
+	RETURN currtables;
+	END; 
+	$$ LANGUAGE plpgsql;
+		
+CREATE OR REPLACE FUNCTION checkReservation()
+RETURNS TRIGGER AS $$ BEGIN
+RAISE NOTICE 'Sorry! Restaurant is over-booked at this timing.'; 
+RETURN NULL;
+END; $$ LANGUAGE plpgsql;
+	
+DROP TRIGGER IF EXISTS trig1 ON public.reservation;
+CREATE TRIGGER trig1
+BEFORE INSERT ON Reservation
+FOR EACH ROW WHEN (NEW.numtables > getCurrTables(NEW.rname,NEW.branchID, NEW.reserveDate, NEW.reserveTime))
+EXECUTE PROCEDURE checkReservation();
